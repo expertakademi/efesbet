ae36e18c24cc9d194f1907f4561c96a05a6b7318
lalitmendapara <lalit@aspiresoftwareconsultancy.com>
2013-07-18 15:36:56 +0530

    Menu implemented. Grid cell click (column 11+)  on click sub grid will open.

--- EfesBetSol/EfesBet.UI/MainWindow.xaml.cs
+++ EfesBetSol/EfesBet.UI/MainWindow.xaml.cs
@@ -34,17 +34,14 @@ namespace EfesBetGUI
         public MainWindow()
         {
             InitializeComponent();//
-            var rowDetailsTemplate = (DataTemplate)this.Resources["rowDT"];
-            dataGridParent.RowDetailsTemplate = rowDetailsTemplate;
-            
-
             this.Height = SystemParameters.MaximizedPrimaryScreenHeight;
             this.Width = SystemParameters.MaximizedPrimaryScreenWidth;
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;            
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             this.miAxleTools.Click += new RoutedEventHandler(miAxleTools_Click);
+
             #region -- ViewModel --
             vmSK = new OwnGuestViewModel();
-            this.DataContext = vmSK;            
+            this.DataContext = vmSK;
             #endregion
             image1.VerticalAlignment = System.Windows.VerticalAlignment.Center;
             image2.VerticalAlignment = System.Windows.VerticalAlignment.Center;
@@ -52,13 +49,99 @@ namespace EfesBetGUI
             colLastOrgPos = grdLastCol.Width;
             spLeft.IsEnabled = false;
             spRight.IsEnabled = false;
-            this.dataGridParent.DataContext = ownguest.GuestHostTotalList;            
-            //this.dataGridParent.RowDetailsVisibilityChanged += new EventHandler<DataGridRowDetailsEventArgs>(dataGridParent_RowDetailsVisibilityChanged);
-            //this.dataGridParent.MouseRightButtonUp += new MouseButtonEventHandler(dataGridParent_MouseRightButtonUp);
-            //this.dataGridParent.MouseLeftButtonUp += new MouseButtonEventHandler(dataGridParent_MouseLeftButtonUp);           
+            this.dataGridParent.DataContext = ownguest.GuestHostTotalList;
+            this.dataGridParent.RowDetailsVisibilityChanged += new EventHandler<DataGridRowDetailsEventArgs>(dataGridParent_RowDetailsVisibilityChanged);
+            // this.dataGridParent.MouseRightButtonUp += new MouseButtonEventHandler(dataGridParent_MouseRightButtonUp);
+            //   this.dataGridParent.MouseLeftButtonUp += new MouseButtonEventHandler(dataGridParent_MouseLeftButtonUp);           
         }
 
+
+
+
+
+
+
         void dataGridParent_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //    //detecting the column, cell and row that has been clicked 
+            //    DependencyObject dep = (DependencyObject)e.OriginalSource;
+
+            //    // iteratively traverse the visual tree
+            //    while ((dep != null) && !(dep is DataGridCell) && !(dep is DataGridColumnHeader))
+            //    {
+            //        dep = VisualTreeHelper.GetParent(dep);
+            //    }
+
+            //    if (dep == null)
+            //        return;
+
+            //    if (dep is DataGridColumnHeader)
+            //    {
+            //        DataGridColumnHeader columnHeader = dep as DataGridColumnHeader;
+            //        // do something
+            //    }
+
+            //    if (dep is DataGridCell)
+            //    {
+            //        DataGridCell cell = dep as DataGridCell;
+
+            //        // do something
+            //        // navigate further up the tree
+            //        while ((dep != null) && !(dep is DataGridRow))
+            //        {
+            //            dep = VisualTreeHelper.GetParent(dep);
+            //        }
+            //        DataGridRow row = dep as DataGridRow;
+            //        DataGridColumn col=dep as DataGridColumn;
+            //        int colIndexValue = cell.Column.DisplayIndex;
+            //        MessageBox.Show(colIndexValue.ToString());
+            //        if (colIndexValue == 10)
+            //        {
+            //            //DataGrid innerDataGrid = new DataGrid();
+            //            //innerDataGrid.Height = 10;
+            //            //innerDataGrid.Width = 200;
+            //            //innerDataGrid.AutoGenerateColumns = false;
+
+            //            //innerDataGrid.DataContext = ownguest.SubGridItemList;
+            //            //DataGrid innerDataGrid = new DataGrid();
+            //           // innerDataGrid.DataContext = ownguest.SubGridItemList;
+
+
+            //        }
+            //        else
+            //        {
+
+            //        }
+            //        int rowIndex = FindRowIndex(row);         
+
+            //        //extract the value of the cell 
+            //        object value=ExtractBoundValue(row, cell);
+            //    }
+            //}
+
+            //private object ExtractBoundValue(DataGridRow row,  DataGridCell cell)
+            //{
+            //    // find the column that this cell belongs to
+            //    DataGridBoundColumn col = cell.Column as DataGridBoundColumn;
+
+            //    // find the property that this column is bound to
+            //    Binding binding = col.Binding as Binding;
+            //    string boundPropertyName = binding.Path.Path;
+
+            //    // find the object that is related to this row
+            //    object data = row.Item;
+
+            //    // extract the property value
+            //    PropertyDescriptorCollection properties =
+            //        TypeDescriptor.GetProperties(data);
+
+            //    PropertyDescriptor property = properties[boundPropertyName];
+            //    object value = property.GetValue(data);
+
+            //    return value;
+        }
+
+        void dataGridParent_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
             //detecting the column, cell and row that has been clicked 
             DependencyObject dep = (DependencyObject)e.OriginalSource;
@@ -81,81 +164,14 @@ namespace EfesBetGUI
             if (dep is DataGridCell)
             {
                 DataGridCell cell = dep as DataGridCell;
+
                 // do something
                 // navigate further up the tree
                 while ((dep != null) && !(dep is DataGridRow))
                 {
                     dep = VisualTreeHelper.GetParent(dep);
                 }
-                DataGridRow row = dep as DataGridRow;
-                DataGridColumn col=dep as DataGridColumn;
-                int colIndexValue = cell.Column.DisplayIndex;
-                MessageBox.Show(colIndexValue.ToString());
-                if (colIndexValue == 10)
-                {
-                    //DataGrid innerDataGrid = new DataGrid(); 
-                    //innerDataGrid.DataContext = ownguest.SubGridItemList;
-                    /*  */
-                }
-                int rowIndex = FindRowIndex(row);         
 
-                //extract the value of the cell 
-                object value=ExtractBoundValue(row, cell);
-            }
-        }
-
-        private object ExtractBoundValue(DataGridRow row,  DataGridCell cell)
-        {
-            // find the column that this cell belongs to
-            DataGridBoundColumn col = cell.Column as DataGridBoundColumn;
-            
-            // find the property that this column is bound to
-            Binding binding = col.Binding as Binding;
-            string boundPropertyName = binding.Path.Path;
-
-            // find the object that is related to this row
-            object data = row.Item;
-
-            // extract the property value
-            PropertyDescriptorCollection properties =
-                TypeDescriptor.GetProperties(data);
-
-            PropertyDescriptor property = properties[boundPropertyName];
-            object value = property.GetValue(data);
-
-            return value;
-        }
-
-        void dataGridParent_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            //detecting the column, cell and row that has been clicked 
-            DependencyObject dep = (DependencyObject)e.OriginalSource;
-
-            // iteratively traverse the visual tree
-            while ((dep != null) && !(dep is DataGridCell) &&  !(dep is DataGridColumnHeader))
-            {
-                dep = VisualTreeHelper.GetParent(dep);
-            }
-
-            if (dep == null)
-                return;
-
-            if (dep is DataGridColumnHeader)
-            {
-                DataGridColumnHeader columnHeader = dep as DataGridColumnHeader;
-                // do something
-            }
-
-            if (dep is DataGridCell)
-            {
-                DataGridCell cell = dep as DataGridCell;                
-                // do something
-                // navigate further up the tree
-                while ((dep != null) && !(dep is DataGridRow))
-                {
-                    dep = VisualTreeHelper.GetParent(dep);
-                }
-                
 
                 DataGridRow row = dep as DataGridRow;
                 //while ((dep != null) && !(dep is DataGridColumn))
@@ -164,7 +180,7 @@ namespace EfesBetGUI
                 //}
 
                 //DataGridColumn col = dep as DataGridColumn;
-                int rowIndex = FindRowIndex(row);
+                // int rowIndex = FindRowIndex(row);
 
             }
         }
@@ -183,21 +199,47 @@ namespace EfesBetGUI
         }
 
 
-        test t = new test();       
+        test t = new test();
 
         void miAxleTools_Click(object sender, RoutedEventArgs e)
         {
             //t.ShowDialog();
             pop.Visibility = Visibility.Visible;
         }
-
         void dataGridParent_RowDetailsVisibilityChanged(object sender, DataGridRowDetailsEventArgs e)
-        {           
+        {
+            DataGrid innerDataGrid = new DataGrid();
+
+            string value = dataGridParent.CurrentColumn.Header.ToString();
+
+            if (value == "+")
+            {
+
+
+               // objDt.Resources.
+               // dataGridParent.RowDetailsTemplate  = new DataTemplate();
+               // objDt.DataType =   DataTable();
+               // dataGridParent.RowDetailsTemplate = objDt;
+
+
+               innerDataGrid = e.DetailsElement as DataGrid;
+                innerDataGrid.DataContext = ownguest.SubGridItemList;
+            }
+
+            else
+            {
+                dataGridParent.SelectedIndex = -1;
+                
+            
+
+            }
+
+            //dataGridParent.Columns.
 
             //MessageBox.Show("Row Visibility");
-            //DataGrid innerDataGrid = e.DetailsElement as DataGrid;
-            //innerDataGrid.DataContext = ownguest.SubGridItemList;
-        }        
+            // DataGrid innerDataGrid = e.DetailsElement as DataGrid;
+            // innerDataGrid.DataContext = ownguest.SubGridItemList;
+        }
         private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (grdFirstCol.Width == new GridLength(0.00))
@@ -213,35 +255,47 @@ namespace EfesBetGUI
                 grdLastCol.Width = new GridLength(0.00);
         }
 
-        private void StackPanel_Loaded(object sender, RoutedEventArgs e)
-        {
 
-            var templateChildPanel = sender as StackPanel;
-            var detailTextBlock = new TextBlock
-            {
-                Width = 350,
-                TextWrapping = TextWrapping.Wrap,
-                Text = "I am Amyo Kabir"
-            };
 
-            //If you need binding
-            //var dataBinding = new Binding("DetailsText");
-            //dataBinding.Mode = BindingMode.TwoWay;
-            //detailTextBlock.SetBinding(TextBlock.TextProperty, dataBinding);
-            templateChildPanel.Children.Add(detailTextBlock);
-        }
 
-       
 
         //private void dataGridParent_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         //{
         //    if (((System.Windows.Controls.DataGrid)(sender)).CurrentColumn.Header.ToString() == "+")
         //    {
 
-                
+
         //    }
         //}
 
-       
+
+
+
+        private void grdBakiyem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            grdBakiyem.Background = new SolidColorBrush(Colors.Gray);
+            Menu1.Visibility = Visibility.Visible;
+        }
+
+        private void grdBakiyem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            grdBakiyem.Background = new SolidColorBrush(Colors.Transparent);
+            Menu1.Visibility = Visibility.Collapsed;
+        }
+
+
+        private void grdSettings_MouseEnter(object sender, MouseEventArgs e)
+        {
+            grdSettings.Background = new SolidColorBrush(Colors.Gray);
+            grdSettingsSubMenu.Visibility = Visibility.Visible;
+            
+        }
+
+        private void grdSettings_MouseLeave(object sender, MouseEventArgs e)
+        {
+            grdSettings.Background = new SolidColorBrush(Colors.Transparent);
+            grdSettingsSubMenu.Visibility = Visibility.Collapsed;
+        }
+
     }
 }
